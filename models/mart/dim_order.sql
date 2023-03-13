2cc90255868355595ce302626c3ca121433fb9ae
Timothy DeWees <timothy.dewees@gmail.com>
2023-03-13 14:41:59 +0000

    Expanded order dimension

--- models/mart/dim_order.sql
+++ models/mart/dim_order.sql
@@ -13,7 +13,13 @@ final as (
 		orders.customer_id as customer_id,
 		orders.shipping_address_id as shipping_address_id,
 		orders.order_date,
-		orders.total
+		orders.total,
+		addresses.address_line_1 as shipping_address_line1,
+		addresses.address_line_2 as shipping_address_line2,
+		addresses.address_line_3 as shipping_address_line3,
+		addresses.city as shipping_address_city,
+		addresses.state as shipping_address_state,
+		addresses.postal_code as shipping_address_postal_code
 	from orders
 	left join customers on orders.customer_id = customers.customer_id
 	left join addresses on orders.shipping_address_id = addresses.address_id
