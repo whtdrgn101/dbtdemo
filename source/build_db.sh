42a58921158c05d5aa3573d3725ff2d4103bfbff
Timothy A. DeWees <timothy.dewees@gmail.com>
2023-03-20 13:35:04 -0400

    Scripted the DB build with a shell script

---
+++ source/build_db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+db_name="$1"
+set echo off
+
+echo "Copying data files to tmp"
+cp ./*.csv /tmp
+
+if [$db_name = ""]; then
+    db_name=template1
+fi
+
+echo "Building Database - $db_name"
+psql -d $db_name < ./source_schema.sql
+
+echo "Loading Data - $db_name"
+psql -d $db_name < ./data_load.sql
