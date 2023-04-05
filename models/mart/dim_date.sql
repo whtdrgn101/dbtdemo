3fd8fa04e374e52c750a7f3ddcc1327482d1b97a
Timothy DeWees <whtdrgn101@users.noreply.github.com>
2023-04-05 13:59:01 -0400

    Update dim_date.sql

--- models/mart/dim_date.sql
+++ models/mart/dim_date.sql
@@ -3,7 +3,7 @@ WITH date_spine AS
 	{{ dbt_utils.date_spine(
 		datepart="day",
 		start_date="to_date('01/01/1950', 'mm/dd/yyyy')",
-		end_date="to_date('12/31/2030','mm/dd/yyy')"
+		end_date="dbt.dateadd(year, 6, current_date)"
 	)
 	}}
 ),
